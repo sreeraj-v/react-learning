d208d14007ef13fc52cebb253a2a6f4cb3b0694d
Sreeraj <srj2255000@gmail.com>
2024-12-26 02:01:13 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const heading = React.createElement("h3", { id: "love" }, "hello sirr!!!!!!!");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "hello parent to child"),
+    React.createElement("h2", {}, "hello parent to child 2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h3", {}, "neated h3"),
+    React.createElement("h4", {}, "nested h4"),
+  ]),
+]);
+
+const rooter = ReactDOM.createRoot(document.getElementById("rooter"));
+
+rooter.render(parent);
+
+console.log(parent);
+console.log(heading);
